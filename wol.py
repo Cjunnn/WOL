393dab0a1d5582ee89b3cee35a0a91263c201859
Cjunnn <james.0.chen@hotmail.com>
2019-05-28 21:10:13 -0400

    update the wake-on-lan source

--- wol.py
+++ wol.py
@@ -1,1 +1,37 @@
-#
+# !/usr/bin/env python333
+# -*- coding: utf-8 -*-
+
+import socket
+import struct
+import time
+
+
+MAC = "4C-ED-FB-3A-A5-4F"
+BROADCAST = "192.168.1.255"
+
+
+def main():
+    if len(MAC) != 17:
+        raise ValueError("MAC address should be set as form 'XX-XX-XX-XX-XX-XX'")
+    mac_address = MAC.replace("-", '')
+    data = ''.join(['FFFFFFFFFFFF', mac_address * 20])
+    send_data = b''
+
+    for i in range(0, len(data), 2):
+        send_data = b''.join([send_data, struct.pack('B', int(data[i: i + 2], 16))])
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.sendto(send_data, (BROADCAST, 7))
+        time.sleep(1)
+        sock.sendto(send_data, (BROADCAST, 7))
+        time.sleep(1)
+        sock.sendto(send_data, (BROADCAST, 7))
+        print("Done")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
